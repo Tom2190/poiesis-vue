a6c4d3100ec3ffb6833a4bb6f00f77ee06c4d64b
Priscila Bey <pri.bey@gmail.com>
2021-06-21 21:09:19 -0300

    ejemplo axios textos

--- src/components/AreaComun/src/componentes/AreaComun.js
+++ src/components/AreaComun/src/componentes/AreaComun.js
@@ -3,9 +3,26 @@ export default {
   components: {},
   props: [],
   data() {
-    return {};
+    return {
+      textos: []
+    };
   },
   computed: {},
-  mounted() {},
-  methods: {},
+  async mounted() {
+    await this.getTexts()
+  },
+  methods: {
+    async getTexts() {
+      console.log('getUsuarios')
+      
+        try {
+          let respuesta = await this.axios(this.url)
+          console.log('AXIOS GET',respuesta.data)
+          this.textos = respuesta.data
+        }
+        catch(error) {
+          console.log(error)
+        }
+    }
+  },
 };
