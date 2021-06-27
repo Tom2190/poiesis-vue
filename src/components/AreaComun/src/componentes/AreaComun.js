1ed91665178fda435316adc22c86b4254a737dce
lola-log <lula.serena@gmail.com>
2021-06-26 21:57:58 -0300

    esqueleto de seccion comun

--- src/components/AreaComun/src/componentes/AreaComun.js
+++ src/components/AreaComun/src/componentes/AreaComun.js
@@ -13,17 +13,8 @@ export default {
     await this.getTexts()
   },
   methods: {
-    async getTexts() {
-      console.log('getUsuarios')
-      
-        try {
-          let respuesta = await this.axios(this.url)
-          console.log('AXIOS GET',respuesta.data)
-          this.textos = respuesta.data
-        }
-        catch(error) {
-          console.log(error)
-        }
+    redirectToGenre(genre){
+      this.$router.push(`/area-comun/${genre}`)
     }
   },
 };
